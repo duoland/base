b2b8ccf839eb6d64f5a047a505eb756f6a0d81b5
jemygraw <jemygraw@gmail.com>
2020-08-02 09:11:44 +0800

    feat(strings): add some common functions to the strings package.

--- utils/strings/strings.go
+++ utils/strings/strings.go
@@ -1,5 +1,7 @@
 package strings
 
+import "fmt"
+
 func SliceContains(slice []string, s string) bool {
 	for _, item := range slice {
 		if item == s {
@@ -25,10 +27,36 @@ func ReverseRunes(str string) (output string) {
 	return
 }
 
+// ReverseString reverse a string content and return the result
+func ReverseString(str string) (output string) {
+	if str == "" {
+		return
+	}
+	buf := make([]byte, 0, len(str))
+	for i := len(str) - 1; i >= 0; i-- {
+		buf = append(buf, str[i])
+	}
+	output = string(buf)
+	return
+}
+
+func Int64ToString(value int64) string {
+	return fmt.Sprintf("%d", value)
+}
+
 func IsNilOrEmpty(s *string) bool {
 	return s == nil || *s == ""
 }
 
 func IsEmpty(s string) bool {
 	return s == ""
+}
+
+func TruncateRuneToSize(str string, maxAllowedSize int) string {
+	chars := []rune(str)
+	if len(chars) <= maxAllowedSize {
+		return str
+	}
+	// otherwise
+	return string(chars[:maxAllowedSize])
 }
