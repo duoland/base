1114056b2d72379bdba335e0fc6c7e55081eafd3
金鑫鑫 <jemygraw@gmail.com>
2025-02-21 18:39:43 +0800

    feat(base32)：添加一个将 base32字符串转为 int64的方法；

--- utils/strings/strings.go
+++ utils/strings/strings.go
@@ -110,10 +110,10 @@ func StringSliceToInt64Slice(valueStrs []string) (values []int64) {
 	return
 }
 
+const Base32Chars = "0123456789abcdefghijklmnopqrstuv"
+
 // Int64ToBase32 将 int64 类型的整数转换为 32 进制字符串
 func Int64ToBase32(num int64) string {
-	// 定义 32 进制字符集
-	base32Chars := "0123456789abcdefghijklmnopqrstuv"
 	// 处理 num 为 0 的特殊情况
 	if num == 0 {
 		return "0"
@@ -125,13 +125,28 @@ func Int64ToBase32(num int64) string {
 		// 计算当前位对应的 32 进制字符的索引
 		remainder := num % 32
 		// 将对应的字符添加到结果构建器中
-		result.WriteByte(base32Chars[remainder])
+		result.WriteByte(Base32Chars[remainder])
 		// 更新 num 的值，进行下一轮取模
 		num /= 32
 	}
 	// 反转结果字符串
 	reversed := result.String()
 	return reverseString(reversed)
+}
+
+// Base32ToInt64 将 32 进制字符串转换为 int64 类型的整数
+func Base32ToInt64(base32Str string) (int64, error) {
+	var result int64
+	for _, char := range base32Str {
+		// 查找字符在 32 进制字符集中的位置
+		index := strings.IndexRune(Base32Chars, char)
+		if index == -1 {
+			return 0, fmt.Errorf("invalid base32 character: %c", char)
+		}
+		// 更新结果值，通过乘以 32 加上当前字符对应的数值
+		result = result*32 + int64(index)
+	}
+	return result, nil
 }
 
 // reverseString 反转字符串
--- utils/strings/strings_test.go
+++ utils/strings/strings_test.go
@@ -5,5 +5,10 @@ import "testing"
 func TestInt64ToBase32(t *testing.T) {
 	var src int64 = 1892882925901320192
 	dst := Int64ToBase32(src)
-	t.Log(dst)
+	t.Log("base32=", dst)
+	src1, _ := Base32ToInt64(dst)
+	if src1 != src {
+		t.Fatal("base32 convert fail")
+	}
+	t.Log("int64=", src1)
 }
